8becfd589d2a66557c31e4d5faea94f0f49dfaf3
Stefan <stefan@poehner24.de>
2017-03-26 11:25:45 +0200

    Project structure and configuration files

---
+++ src/JsonStreamParser/JsonStreamParser.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace JsonStreamParser;
+
+/**
+ * @author  Stefan Pöhner <github@poe-php.de>
+ * @license MIT
+ *
+ * @package JsonStreamParser
+ */
+class JsonStreamParser
+{
+
+}
